b6fb31c86bbe4c141c97329f6287d95c1a5122d9
Funke <roiegol9@gmail.com>
2022-11-24 23:24:43 +0200

    Minor change.

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,12 +1,6 @@
 import { bootstrap } from "./bootstrap";
 import networkConfigs from "./config/networkConfigs.json";
 import driverInitializer from "./db";
-import ConnectedUserMap from "./data/ConnectedUserMap";
-import { DataHandeler } from "./data/DataHandeler";
-import NetworkLayer from "./common/networkLayer";
-import { SocketsManagerObserver } from "./common/networkLayer/SocketMenegerObserver";
-
-export const networkLayer = new NetworkLayer();
 
 bootstrap(
     {
